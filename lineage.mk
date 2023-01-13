8b5c5a7bd2493a5f135154f86983ce99900c5948
Mithil Bavishi <bavishimithil@gmail.com>
2023-01-13 14:25:42 +0530

    Add Launcher3Go, nano and terminal

--- lineage.mk
+++ lineage.mk
@@ -35,3 +35,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=espresso10wifixx \
     BUILD_FINGERPRINT="samsung/espresso10wifixx/espresso10wifi:4.2.2/JDQ39/P5110XXDML1:user/release-keys" \
     PRIVATE_BUILD_DESC="espresso10wifixx-user 4.2.2 JDQ39 P5110XXDML1 release-keys"
+
+# Device & filesystem
+PRODUCT_PACKAGES += \
+    Launcher3Go \
+    Terminal \
+    nano
