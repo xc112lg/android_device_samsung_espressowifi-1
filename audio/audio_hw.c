92f0e55e336b62ddaae014809d61baa38d374740
Mithil Bavishi <bavishimithil@gmail.com>
2023-02-02 12:15:05 +0530

    audio: audio_hw.c set application target sdk version to fix audio hal.

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -41,8 +41,11 @@
 #include <hardware/audio_effect.h>
 #include <audio_effects/effect_aec.h>
 
+#include <android/api-level.h>
 #include "audio_hw.h"
 #include "ril_interface.h"
+
+extern void android_set_application_target_sdk_version(uint32_t target);
 
 struct pcm_config pcm_config_mm = {
     .channels = 2,
@@ -2830,6 +2833,8 @@ static int adev_open(const hw_module_t* module, const char* name,
 {
     struct espresso_audio_device *adev;
     int i, ret;
+
+    android_set_application_target_sdk_version(__ANDROID_API_L_MR1__);
 
     if (strcmp(name, AUDIO_HARDWARE_INTERFACE) != 0)
         return -EINVAL;
